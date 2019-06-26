113a0b412c929d1869d1264777ebc448a43e8626
Jonathan Moore <jdm7dv@gmail.com>
2019-06-26 12:47:02 -0400

    Initial Commit

---
+++ Lex/Lex/Accept.cs
@@ -0,0 +1,53 @@
+/*
+ * Class: Accept
+ */
+using System;
+using System.Data;
+
+public class Accept
+{
+/*
+ * Member Variables
+ */
+public string action
+  {
+  get { return astr; }
+  }
+public int line_number
+  {
+  get { return line; }
+  }
+
+string astr;
+int line;
+
+/*
+ * Function: Accept
+ */
+public Accept(String a, int n)
+  {
+  astr = a;
+  line = n;
+  }
+
+public Accept(Accept a)
+  {
+  astr = a.action;
+  line = a.line_number;
+  }
+
+/*
+ * Function: mimic
+ */
+public void mimic(Accept a)
+  {
+  astr = a.action;
+  }
+
+public void dump()
+  {
+   
+  Console.WriteLine("Line:"+ line_number.ToString() +":"+action);
+  }
+
+}
